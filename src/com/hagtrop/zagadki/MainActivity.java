96c07efb0bacdfc38006531051c4dc2cbf81d13a
Hagtrop <volverston@gmail.com>
2014-08-27 09:52:53 +0400

    4 buttons

--- src/com/hagtrop/zagadki/MainActivity.java
+++ src/com/hagtrop/zagadki/MainActivity.java
@@ -2,12 +2,19 @@ package com.hagtrop.zagadki;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	Button simpleBtn, variantsBtn, countdownBtn, sandsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        simpleBtn = (Button) findViewById(R.id.a0_simpleBtn);
+        variantsBtn = (Button) findViewById(R.id.a0_variantsBtn);
+        countdownBtn = (Button) findViewById(R.id.a0_countdownBtn);
+        sandsBtn = (Button) findViewById(R.id.a0_sandsBtn);
     }
 }
